6fd1484701bf7d13ff199f02b7820882b55319a9
Алексей <ejuk@MACBOOKPRO.station>
2024-11-27 11:10:26 +0000

    update

---
+++ src/components/Modal.vue
@@ -0,0 +1,41 @@
+<template>
+    <teleport to=".modals-container">
+        <div v-if="modelValue" class="modal">
+            <h1>
+                {{ title }}
+            </h1>
+            <slot />
+            <button @click="$emit('update:modelValue', false)">Hide modal</button>
+        </div>
+    </teleport>
+</template>
+
+<script setup>
+const props = defineProps({
+    modelValue: {
+        type: Boolean,
+        default: false
+    },
+    title: {
+        type: String,
+        default: 'No title specified'
+    }
+})
+
+const emit = defineEmits(['update:modelValue'])
+// const handleButtonClick = () => {
+//     emit('update:modelValue', false);
+// }
+</script>
+
+<style>
+.modal {
+    background: beige;
+    padding: 10px;
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
+</style>
